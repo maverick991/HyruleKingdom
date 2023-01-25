08e2ce80b56c8d4f7eead86a5df424add29acc4c
MaverickEdwards <r.edwards0917@gmail.com>
2023-01-25 14:35:03 -0500

    add form

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function sayHi(greeting){
-console.log("Hello World")
-}
+let addCreature = false;
 
-greeting()
+document.addEventListener("DOMContentLoaded", () => {
+    const addBtn = document.querySelector("new-creature-btn");
+    const creatureFormContainer =  document.querySelector(".container");
+    addBtn.addEventListener("click", () => {
+        addCreature = !addCreature;
+        if (addCreature){
+            creatureFormContainer.style.display = "block";
+        } else {
+            creatureFormContainer.style.display = "none";
+        }
+    });
+})
+
+const creatureUrl = 'http://localhost:3000/creatures'
